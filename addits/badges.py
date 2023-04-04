d92e5328166886dc66eb744ebc80c805c6c92211
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2023-04-04 16:20:36 +0600

    + update badges logic & test

--- addits/badges.py
+++ addits/badges.py
@@ -1,36 +1,42 @@
-# Source: https://github.com/dmachard/blocklist-domains
+#!/usr/bin/env python3
+# Author: Yevgeniy Goncharov, https://lab.sys-adm.in
+# Ref: https://github.com/dmachard/blocklist-domains
+import os
 
 from pybadges import badge
 from datetime import date
 
-# create badge pour the date
-b1 = badge(left_text='Last updated', 
-          right_text="%s" % date.today(),
-          right_color='blue')
-          
-with open("badge_date.svg", "w") as f:
-    f.write(b1)
-    
-# create badge pour the number of hosts
-with open("blocklist.txt", "r") as f:
-    data = f.read()
-domains = data.splitlines()
+os.chdir("../public/files")
 
-b2 = badge(left_text='Blocklisted hosts count',
-           right_text="%s" % len(domains),
-           right_color='red')
-           
-with open("badge_total.svg", "w") as f:
-    f.write(b2)
 
-# create badge wl the number of hosts
-with open("allowlist.txt", "r") as f:
-    data = f.read()
-wldomains = data.splitlines()
+def create_date_badge():
+    b1 = badge(left_text='Last updated',
+               right_text="%s" % date.today(),
+               right_color='blue')
 
-a1 = badge(left_text='Whitelisted hosts count',
-           right_text="%s" % len(wldomains),
-           right_color='green')
+    with open("badge_date.svg", "w") as f:
+        f.write(b1)
 
-with open("badge_total_allow.svg", "w") as f:
-    f.write(a1)
+
+def create_list_badge(list_name, list_description, file_badge, color_badge):
+    # bl badge
+    with open(list_name, "r") as f:
+        list_data = f.read()
+    list_elements = list_data.splitlines()
+
+    badge_data = badge(left_text=list_description,
+                       right_text="%s" % len(list_elements),
+                       right_color=color_badge)
+
+    with open(file_badge, "w") as f:
+        f.write(badge_data)
+
+
+# Generate badges
+create_date_badge()
+create_list_badge("bl.txt", "BL count", "badge_total_bl.svg", "red")
+create_list_badge("bl_plain.txt", "BL with regex count", "badge_total_bl_plain.svg", "red")
+create_list_badge("wl.txt", "WL count", "badge_total_wl.svg", "green")
+create_list_badge("wl_plain.txt", "WL with regex count", "badge_total_wl_plain.svg", "green")
+create_list_badge("ip_plain.txt", "IP count", "badge_total_ip.svg", "red")
+
